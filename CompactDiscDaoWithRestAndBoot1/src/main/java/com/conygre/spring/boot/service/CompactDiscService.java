e8d3c872d6d81c9f00ceb4b57edf07e64d431cba
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2022-08-17 14:00:40 -0400

    removed default project

--- CompactDiscDaoWithRestAndBoot1/src/main/java/com/conygre/spring/boot/service/CompactDiscService.java
+++
@@ -1,9 +0,0 @@
-package com.conygre.spring.boot.service;
-
-import com.conygre.spring.boot.entities.CompactDisc;
-
-import java.util.List;
-
-public interface CompactDiscService {
-    List<CompactDisc> getCatalog();
-}
