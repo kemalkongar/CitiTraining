76299c841fa856b051295f2a5494a748211f462d
Xinchen Hou <65501463+Dora0523@users.noreply.github.com>
2022-08-24 18:36:09 -0400

    problem in plot -- coudn't receive data from api

--- myApp/src/app/plot/plot.component.ts
+++ myApp/src/app/plot/plot.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit,Input } from '@angular/core';
 import { Security } from '../security';
 import { SecurityService } from '../security.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { AfterViewInit, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { IgxLegendComponent, IgxCategoryChartComponent } from 'igniteui-angular-charts';
 @Component({
   selector: 'app-plot',
   templateUrl: './plot.component.html',
@@ -10,8 +11,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class PlotComponent implements OnInit {
   mySecurities!: Security[];
+  oneSecurity!:Security [];
   securityService: SecurityService;
-  
   constructor(private http: HttpClient) {
     this.securityService = new SecurityService(http);
   }
@@ -24,5 +25,21 @@ export class PlotComponent implements OnInit {
   }, error => {
     console.log('error');
   });
-}}
+}
+
+  @ViewChild("legend", { static: true } )
+  private legend: IgxLegendComponent
+  @ViewChild("chart", { static: true } )
+  private chart: IgxCategoryChartComponent
+
+  selectSecurity(id:number): void {
+    this.securityService?.findByID(id).subscribe((data) => {
+      console.log(data);
+      this.oneSecurity = data;
+  }, error => {
+    console.log('error');
+  });
+}
+}
+
 
--- myApp/src/app/security.service.ts
+++ myApp/src/app/security.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import{ GlobalConstants } from './global-constants';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/timeout';
+import { identifierModuleUrl } from '@angular/compiler';
 // import { map } from 'rxjs/operators';
 // import { timeout } from 'rxjs/operators';
 
@@ -14,6 +15,7 @@ import 'rxjs/add/operator/timeout';
 export class SecurityService {
 
   private getAllUrl: string; 
+
   private requestMapping='api/securities/';
   tmp:Observable<Security[]>;
   
@@ -24,6 +26,16 @@ export class SecurityService {
   public findAll():Observable<Security[]>{
     return this.http?.get<Security[]>(this.getAllUrl);
   }
+
+  public findByID(id: number):Observable<Security[]>{
+    let getByIDUrl:string = this.getAllUrl+id;
+    // return this.http?.get<Security[]>('http://miss2trainingproject-miss2trainingproject.openshift82.conygre.com/api/securities/1');
+    return this.http?.get<Security[]>(getByIDUrl);
+  }
+  // we don't ever save a security or a ticker
+  // public save(s: Security): Observable<Object> {
+  //   return this.http.post<Security>(this.addUrl, s);
+  // }
 
 
 }
