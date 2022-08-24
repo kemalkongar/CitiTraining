5c8ed0fbaf18415054b4f9d129d3a43760bc2db2
Xinchen Hou <65501463+Dora0523@users.noreply.github.com>
2022-08-24 15:25:04 -0400

    update css and graph

--- myApp/src/app/plot/plot.component.ts
+++ myApp/src/app/plot/plot.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Security } from '../security';
+import { SecurityService } from '../security.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-plot',
   templateUrl: './plot.component.html',
   styleUrls: ['./plot.component.css']
 })
-export class PlotComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class implements OnInit {
+  mySecurities!: Security[];
+  securityService: SecurityService;
+  
+  constructor(private http: HttpClient) {
+    this.securityService = new SecurityService(http);
   }
 
-}
+  ngOnInit(): void {
+
+    this.securityService?.findAll().subscribe((data) => {
+      console.log(data);
+      this.mySecurities = data;
+  }, error => {
+    console.log('error');
+  });
+}}
+
