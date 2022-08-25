8392098fd21ecf0a73a65cd918f9d368504b2d80
Weiyu Li <84486708+duoduocai-dot@users.noreply.github.com>
2022-08-25 14:34:16 -0400

    add order form components

---
+++ myApp/src/app/order-form/order-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OrderService } from '../order.service';
+import { Order } from '../order';
+
+@Component({
+  selector: 'app-order-form',
+  templateUrl: './order-form/order-form.component.html',
+  styleUrls: ['./order-form/order-form.component.css']
+})
+export class OrderFormComponent implements OnInit {
+
+  order: Order;
+
+  constructor(
+    private route: ActivatedRoute, 
+      private router: Router, 
+        private orderService: OrderService) {
+    // this.order = new Order();
+  }
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.orderService.save(this.order).subscribe(result => this.refreshOrdersAndQueueTable());
+  }
+
+  refreshOrdersAndQueueTable() {
+    this.router.navigate([this.orderService.getAllUrl]);
+    this.router.navigate([this.orderService.getQueueUrl]);
+
+  }
+
+}
